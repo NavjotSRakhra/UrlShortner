c0b8b09f5cc9e4f05343533bdb6eb50e8a0749f4
Navjot Singh Rakhra <45345158+NavjotSRakhra@users.noreply.github.com>
2023-11-16 00:54:14 +0530

    Added barebones frontend

--- src/main/java/io/github/navjotsrakhra/urlshortner/controller/ShortenedUrlRedirectionController.java
+++ src/main/java/io/github/navjotsrakhra/urlshortner/controller/ShortenedUrlRedirectionController.java
@@ -1,6 +1,7 @@
 package io.github.navjotsrakhra.urlshortner.controller;
 
 import io.github.navjotsrakhra.urlshortner.service.ShortenedUrlRedirectionService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,5 +21,13 @@ public class ShortenedUrlRedirectionController {
     @GetMapping("/{key}")
     public ResponseEntity<Void> redirect(@PathVariable String key) {
         return shortenedUrlRedirectionService.redirect(key);
+    }
+
+    @GetMapping
+    public ResponseEntity<Void> redirect() {
+        return ResponseEntity
+                .status(HttpStatus.SEE_OTHER)
+                .header("Location", "/static/index.html")
+                .build();
     }
 }
