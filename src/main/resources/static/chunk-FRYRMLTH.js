be28f1db436a8f35e2547ef74b38040dc6593f9f
Navjot Singh Rakhra <45345158+NavjotSRakhra@users.noreply.github.com>
2023-11-18 20:59:53 +0530

    Fixed DockerFile (Using JDK20 instead of JDK21), Updated UI

---
+++ src/main/resources/static/chunk-FRYRMLTH.js
@@ -0,0 +1,1 @@
+import{a}from "./chunk-KFYINUWP.js";import"./chunk-MXQOUSZU.js";import"./chunk-LNQBIKWW.js";import"./chunk-BKILB7EO.js";export{a as ManageComponent};
--- src/main/resources/static/chunk-SAC2XBGI.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-PE6URQAN.js";import"./chunk-46MWDKRC.js";import"./chunk-U4Z7WID7.js";export{a as HomeComponent};
