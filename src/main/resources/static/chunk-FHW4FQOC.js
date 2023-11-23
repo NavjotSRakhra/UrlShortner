1a8e556de9c06df6dbcbf2bf124defd98f6bc4d1
Navjot Singh Rakhra <45345158+NavjotSRakhra@users.noreply.github.com>
2023-11-23 15:33:55 +0530

    Fixed PWA configuration (v1.0.3)

--- src/main/resources/static/chunk-FHW4FQOC.js
+++ src/main/resources/static/chunk-FHW4FQOC.js
@@ -1,1 +1,1 @@
-import{a}from "./chunk-FRXB3YGR.js";import"./chunk-XBJKUZ2Z.js";import"./chunk-D2BBXM5P.js";import"./chunk-NMG2UWF5.js";export{a as ManageComponent};
+import{a}from"./chunk-FRXB3YGR.js";import"./chunk-XBJKUZ2Z.js";import"./chunk-D2BBXM5P.js";import"./chunk-NMG2UWF5.js";export{a as ManageComponent};
