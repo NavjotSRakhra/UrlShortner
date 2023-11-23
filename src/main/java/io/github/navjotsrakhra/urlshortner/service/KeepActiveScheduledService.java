cd16ef3f24a963e32506fda62a6928d4f1707b96
Navjot Singh Rakhra <45345158+NavjotSRakhra@users.noreply.github.com>
2023-11-23 15:13:35 +0530

    Updated the service calling the base url periodically to use Apache HttpClient

--- src/main/java/io/github/navjotsrakhra/urlshortner/service/KeepActiveScheduledService.java
+++ src/main/java/io/github/navjotsrakhra/urlshortner/service/KeepActiveScheduledService.java
@@ -1,14 +1,27 @@
 package io.github.navjotsrakhra.urlshortner.service;
 
 
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+@Component
 public class KeepActiveScheduledService {
-    @Scheduled(fixedRate = 10 * 60 * 1000)
+    @Scheduled(fixedDelay = 10, timeUnit = TimeUnit.MINUTES)
     public void keepActive() {
-        String url = System.getenv("base_url");
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getForObject(url, String.class);
+        var url = System.getenv("base_url");
+
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().setDefaultRequestConfig(RequestConfig.DEFAULT).build()) {
+            HttpGet get = new HttpGet(url);
+            var response = httpClient.execute(get);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
