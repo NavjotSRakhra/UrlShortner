cd5bca7c98924c5ec00f3c757091dee153bef3d5
Navjot Singh Rakhra <45345158+NavjotSRakhra@users.noreply.github.com>
2023-11-24 21:48:27 +0530

    Fixed compilation error

--- src/main/java/io/github/navjotsrakhra/urlshortner/service/KeepActiveScheduledService.java
+++ src/main/java/io/github/navjotsrakhra/urlshortner/service/KeepActiveScheduledService.java
@@ -20,7 +20,7 @@ public class KeepActiveScheduledService {
         try (CloseableHttpClient httpClient = HttpClientBuilder.create().setDefaultRequestConfig(RequestConfig.DEFAULT).build()) {
             HttpGet get = new HttpGet(url);
             var response = httpClient.execute(get);
-            System.out.println(new String(response.getEntity().readAllBytes());
+            System.out.println(response.getEntity().getContent().toString());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
