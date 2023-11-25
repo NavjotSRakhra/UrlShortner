de88015809c2e8aa0cddb8f7f7de9ad73406b353
Navjot Singh Rakhra <45345158+NavjotSRakhra@users.noreply.github.com>
2023-11-26 00:34:10 +0530

    Fixed Repeated tasks

--- src/main/java/io/github/navjotsrakhra/urlshortner/config/KeepActiveScheduledConfig.java
+++ src/main/java/io/github/navjotsrakhra/urlshortner/config/KeepActiveScheduledConfig.java
@@ -1,16 +1,18 @@
-package io.github.navjotsrakhra.urlshortner.service;
+package io.github.navjotsrakhra.urlshortner.config;
 
 
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-public class KeepActiveScheduledService {
+@Configuration
+public class KeepActiveScheduledConfig {
     @Scheduled(fixedDelay = 10, timeUnit = TimeUnit.MINUTES)
     public void keepActive() {
         var url = System.getenv("base_url");
@@ -18,7 +20,6 @@ public class KeepActiveScheduledService {
         try (CloseableHttpClient httpClient = HttpClientBuilder.create().setDefaultRequestConfig(RequestConfig.DEFAULT).build()) {
             HttpGet get = new HttpGet(url);
             var response = httpClient.execute(get);
-            System.out.println(response.getEntity().getContent().toString());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
