bc597d6b5b74372bbd54a9d3d1c992de805f28f0
Navjot Singh Rakhra <45345158+NavjotSRakhra@users.noreply.github.com>
2024-02-17 20:31:19 +0530

    Updated UI from material 2 -> Material 3

---
+++ src/main/resources/static/chunk-EIQ6OQTG.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-SY45GUH4.js";import"./chunk-AU5K3ZSJ.js";import"./chunk-W23EXF56.js";import"./chunk-HDNIJOCR.js";export{a as ManageComponent};
---
+++ src/main/resources/static/chunk-KGZOWUNO.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-PKAO5TXA.js";import"./chunk-AU5K3ZSJ.js";import"./chunk-HDNIJOCR.js";export{a as HomeComponent};
